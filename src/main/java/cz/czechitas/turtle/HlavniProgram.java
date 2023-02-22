1a4ab1ba34c143caca994a40ece9359eebabc85b
Marcela Pernicová <marcela.pernicova4@gmail.com>
2023-02-22 22:40:45 +0100

    zmrzlina a sněhulák

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -8,27 +8,64 @@ public class HlavniProgram {
     private Turtle zofka = new Turtle();
 
     public void start() {
-        zofka.setLocation(70,80);
+
+
+        zofka.setLocation(70, 80);
         kolecko(Color.blue);
 
-        zofka.setLocation(128,220);
-        trojuhelnik(Color.ORANGE,140);
+        zofka.setLocation(128, 220);
+        trojuhelnik(Color.ORANGE, 140);
+
+
+
+        for (int i = 0; i < 3; i++) {
+            koleckoSnehulak(Color.cyan, 320, 80 + 70 * i,200*(i+1));
+            zofka.turnRight(32);
+
+        }
+        zofka.setLocation(290, 160);
+        ruceSnehulaka(Color.blue);
+        zofka.setLocation(460, 100);
+        ruceSnehulaka(Color.blue);
     }
 
     public static void main(String[] args) {
         new HlavniProgram().start();
     }
 
-    public void kolecko(Color color){
+
+    public void kolecko(Color color) {
         zofka.setPenColor(color);
-        int jemnostKruhu = 120;
-        int obvodKruhu = 360;
-        for(int i=0; i < jemnostKruhu; i++) {
-            zofka.turnRight(obvodKruhu /jemnostKruhu);
-            zofka.move(360/jemnostKruhu);
+        double jemnostKruhu = 120;
+        double obvodKruhu = 360;
+        for (int i = 0; i < jemnostKruhu; i++) {
+            zofka.turnRight(360 / jemnostKruhu);
+            zofka.move(obvodKruhu / jemnostKruhu);
         }
+    }
+    public void koleckoSnehulak(Color color, int zacinajiciX, int zacinajiciY, int obvodKruhu) {
+        zofka.setLocation(zacinajiciX, zacinajiciY);
+        zofka.setPenColor(color);
+        double jemnostKruhu = 120;
+        for (int i = 0; i < jemnostKruhu; i++) {
+            zofka.turnRight(360 / jemnostKruhu);
+            zofka.move(obvodKruhu / jemnostKruhu);
         }
-    public void trojuhelnik (Color color,double velikostStrany){
+
+    }
+    public void ruceSnehulaka (Color color){
+        zofka.setPenColor(color);
+        double jemnostKruhu = 100;
+        double obvodKruhu = 120;
+        for (int i = 0; i < jemnostKruhu; i++) {
+            zofka.turnRight(360 / jemnostKruhu);
+            zofka.move(obvodKruhu / jemnostKruhu);
+        }
+    }
+
+
+
+    public void trojuhelnik(Color color, double velikostStrany) {
         zofka.setPenColor(color);
         zofka.turnLeft(30);
         zofka.move(velikostStrany);
